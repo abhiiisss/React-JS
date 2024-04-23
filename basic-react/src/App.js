b0eda07f5918afcd5ca907f70f84e0c05c234f18
abhiiiss <abhissharma2281@gmail.com>
2024-04-23 19:00:28 +0530

    basics of react

---
+++ basic-react/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <h1>
+      Abhishek Sharma
+    </h1>
+  );
+}
+
+export default App;
---
+++ vite-react/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Vite w React</h1>
+  )
+}
+
+export default App
