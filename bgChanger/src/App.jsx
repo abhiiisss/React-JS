3f33e187ad003c024e0f7bfe4e81875105ac0fef
abhiiiss <abhissharma2281@gmail.com>
2024-05-01 00:58:41 +0530

    new changes

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -3,15 +3,50 @@ import { useState } from 'react'
 
 
 function App() {
-  const [color, setColor] = useState('olive')
+  const [color, setColor] = useState('black')
 
   return (
-    <>
-      <div className='w-full h-full duration-200'
-      style={{backgroundColor: color}}
+      <div className='w-full h-screen duration-200'
+      style = {{backgroundColor: color}}
       >
-      </div>
-    </>
+        <div className='fixed flex flex-wrap 
+        justify-center bottom-12 inset-x-0 px-2' >
+          <div className='flex flex-wrap justify-center 
+          gap-3 shadow-lg bg-white px-3 py-2 rounded-xl'>
+            <button
+            onClick={() => {setColor("red")}}
+            className='outline-none px-4 py-1 
+            rounded-full text-white shadow-lg'
+            style={{backgroundColor: 'red'}}
+            >RED</button>
+            <button
+            onClick={() => {setColor("blue")}}
+            className='outline-none px-4 py-1 
+            rounded-full text-white shadow-lg'
+            style={{backgroundColor: 'blue'}}
+            >BLUE</button>
+            <button
+            onClick={() => {setColor("olive")}}
+            className='outline-none px-4 py-1 
+            rounded-full text-white shadow-lg'
+            style={{backgroundColor: 'olive'}}
+            >OLIVE</button>
+            <button
+            onClick={() => {setColor("green")}}
+            className='outline-none px-4 py-1 
+            rounded-full text-white shadow-lg'
+            style={{backgroundColor: 'green'}}
+            >GREEN</button>
+            <button
+            onClick={() => {setColor("pink")}}
+            className='outline-none px-4 py-1 
+            rounded-full text-white shadow-lg'
+            style={{backgroundColor: 'pink'}}
+            >PINK</button>
+          </div>
+          </div>
+      </div> 
+    
   )
 }
 
--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react"
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Vite w React</h1>
+    <h1 
+    style={{color: 'black'}}
+    >Vite w React</h1>
   )
 }
 
